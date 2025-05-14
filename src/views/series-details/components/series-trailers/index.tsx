3e9973a2e93baf9d2c75259e3339efec5eb7c4da
BaGreal2 <artemonik17@gmail.com>
2025-05-14 23:41:25 +0200

    fix: Fix trailer display

--- src/views/series-details/components/series-trailers/index.tsx
+++ src/views/series-details/components/series-trailers/index.tsx
@@ -14,13 +14,13 @@ const SeriesTrailers = ({ trailers }: Props) => {
 	const [currentTrailer, setCurrentTrailer] = useState<number>(0);
 
 	return (
-		<div className="flex w-fit flex-col">
+		<div className="flex w-[680px] flex-col">
 			<div className="flex h-8 w-full items-end">
-				{trailers.slice(0, 3).map((trailer, index) => (
+				{trailers.slice(0, 5).map((trailer, index) => (
 					<button
 						key={index}
 						className={cn(
-							'max-w-[30%] min-w-44 cursor-pointer rounded-t-md bg-gray-600/40 px-4 text-white backdrop-blur-md duration-150 ease-in-out',
+							'w-full min-w-32 cursor-pointer rounded-t-md bg-gray-600/40 px-4 text-white backdrop-blur-md duration-150 ease-in-out',
 							index === currentTrailer
 								? 'h-8 bg-gray-800/40'
 								: 'h-6 bg-gray-600/40'
@@ -38,9 +38,9 @@ const SeriesTrailers = ({ trailers }: Props) => {
 					</button>
 				))}
 			</div>
-			<div className="aspect-video h-96 w-fit overflow-hidden rounded-md">
+			<div className="aspect-video w-full overflow-hidden rounded-md">
 				<iframe
-					className="aspect-video h-full"
+					className="aspect-video h-full w-full"
 					src={`https://www.youtube.com/embed/${trailers[currentTrailer].key}`}
 					title="YouTube video player"
 					allowFullScreen
