ed3182f4565fe22444197d49d03aa367b601a8eb
BaGreal2 <artemonik17@gmail.com>
2025-06-14 01:14:25 +0200

    feat: Update hover on user reaction buttons

--- src/views/series-details/components/user-reaction/index.tsx
+++ src/views/series-details/components/user-reaction/index.tsx
@@ -4,11 +4,19 @@ const UserReaction = () => {
 			className="fade-in-top flex items-center gap-1.5 opacity-0"
 			style={{ animationDelay: '50ms' }}
 		>
-			<button className="cursor-pointer rounded-full transition-all duration-300 ease-in-out hover:scale-110 hover:shadow-[0_0_15px_rgba(108,135,84,0.2)]">
-				<img src="/images/like.png" alt="Like" className="size-9" />
+			<button className="cursor-pointer rounded-full transition-all duration-300 ease-in-out hover:scale-110 hover:shadow-[0_0_15px_rgba(108,135,84,0.3)]">
+				<img
+					src="/images/like.png"
+					alt="Like"
+					className="pointer-events-none size-9"
+				/>
 			</button>
-			<button className="cursor-pointer rounded-full transition-all duration-300 ease-in-out hover:scale-110 hover:shadow-[0_0_15px_rgba(157,88,64,0.2)]">
-				<img src="/images/dislike.png" alt="Like" className="size-9" />
+			<button className="cursor-pointer rounded-full transition-all duration-300 ease-in-out hover:scale-110 hover:shadow-[0_0_15px_rgba(157,88,64,0.3)]">
+				<img
+					src="/images/dislike.png"
+					alt="Like"
+					className="pointer-events-none size-9"
+				/>
 			</button>
 		</div>
 	);
