cfb10efe53c0e8ffc0ffa589b7b682527b2aa6b2
BaGreal2 <artemonik17@gmail.com>
2025-06-18 19:13:55 +0200

    feat: Implement search render

--- src/lib/roots.ts
+++ src/lib/roots.ts
@@ -1,2 +1,5 @@
 export const modalRoot =
 	typeof window !== 'undefined' ? document.getElementById('modal-root') : null;
+
+export const searchRoot =
+	typeof window !== 'undefined' ? document.getElementById('search-root') : null;
